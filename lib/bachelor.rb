c9ab4f7d54a56531e7309474ab3f58a28c2f4da4
Justin Wu <justinhwu95@gmail.com>
2019-03-21 01:51:03 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,7 +37,7 @@ def get_occupation(data, hometown)
   total = 0 
   data.each do |a,b|
     b.each do |c|
-      t+= c["age"].to_f
+      total+= c["age"].to_f
 end
 
 def get_average_age_for_season(data, season)
