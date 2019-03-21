50ea2b9880f74a36bedc4802b4eef044f19b0ec1
Justin Wu <justinhwu95@gmail.com>
2019-03-21 01:37:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
   array = []
   data[season].each do |a|
     if a["status"] == "Winner"
-      
+      array = a["status"]
 end
 
 def get_contestant_name(data, occupation)
