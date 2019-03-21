46147515dea7ef76633058fc2fe108f1a29bb3af
Justin Wu <justinhwu95@gmail.com>
2019-03-21 01:30:30 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   # code here
   data[season].each do |a,b|
     b.each do |c,d|
-      if c[:status]
+      if c[:status]has_value?()
     end
   end
 end
