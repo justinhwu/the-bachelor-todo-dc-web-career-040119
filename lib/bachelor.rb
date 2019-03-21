7af70969fafc0ae735c96a2fd5e76b2304d840dd
Justin Wu <justinhwu95@gmail.com>
2019-03-21 01:39:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -12,7 +12,7 @@ def get_contestant_name(data, occupation)
   # code here
   data.each do |a,b|
     if b["occupation"] == occupation
-      
+      return b["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
