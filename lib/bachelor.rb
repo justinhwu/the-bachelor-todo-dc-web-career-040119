9f5df4af53d66a2f2b8f36cff6d2ca85a23d33b8
Justin Wu <justinhwu95@gmail.com>
2019-03-21 01:31:23 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,6 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each do |a,b|
+  data[season].value. do |a,b|
     b.each do |c,d|
       if c[:status]has_value?("Winner")
         
