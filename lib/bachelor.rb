6f95258fb3a1a466c2757425239c51200aa06da5
Justin Wu <justinhwu95@gmail.com>
2019-03-21 01:54:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,7 +45,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |a|
     a.each do |b|
       b["age"].to_f
-      total+= b["age"].to_f
+      total+= b["age"]
       num+=1
     end
   end
